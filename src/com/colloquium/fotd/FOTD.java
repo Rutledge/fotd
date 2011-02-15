35749f1707495876bad1e8d60a3323644f5fda83
Dest <xdestroyer12@gmail.com>
2011-02-15 00:24:05 -0500

    added comment

--- src/com/colloquium/fotd/FOTD.java
+++ src/com/colloquium/fotd/FOTD.java
@@ -9,5 +9,7 @@ public class FOTD extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //herp
     }
 }

0e27b0ed9e279e21eae39708aaeaae8cd921c42d
Dest <xdestroyer12@gmail.com>
2011-02-14 23:59:57 -0500

    First version

---
+++ src/com/colloquium/fotd/FOTD.java
@@ -0,0 +1,13 @@
+package com.colloquium.fotd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FOTD extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
